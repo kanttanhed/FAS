68b4ffecef2a07cdadb5eca2ce6c6813e6e3b081
kanttanhed <plinio.salazar@yahoo.com.br>
2022-12-30 12:03:13 -0300

    Initial Setup

---
+++ salazarmoney-api/src/test/java/com/salazar/salazarmoneyapi/SalazarmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salazar.salazarmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalazarmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
