431f74504ac2d2ddb3ff261cdab0503e83e920fa
kanttanhed <plinio.salazar@yahoo.com.br>
2023-03-04 14:07:35 -0300

    Using events to add header location

--- salazarmoney-api/src/main/java/com/salazar/salazarmoneyapi/controller/CategoriaController.java
+++ salazarmoney-api/src/main/java/com/salazar/salazarmoneyapi/controller/CategoriaController.java
@@ -2,9 +2,12 @@ package com.salazar.salazarmoneyapi.controller;
 
 import com.salazar.salazarmoneyapi.domain.model.Categoria;
 import com.salazar.salazarmoneyapi.domain.repository.CategoriaRepository;
+import com.salazar.salazarmoneyapi.event.RecursoCriadoEvent;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +23,9 @@ public class CategoriaController {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public ResponseEntity<?>  listarTudo() {
         List<Categoria> categorias = categoriaRepository.findAll();
@@ -30,11 +36,8 @@ public class CategoriaController {
     public ResponseEntity<Categoria> criar(@Valid @RequestBody  Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
     //@GetMapping("/{codigoId}")
--- salazarmoney-api/src/main/java/com/salazar/salazarmoneyapi/controller/PessoaController.java
+++ salazarmoney-api/src/main/java/com/salazar/salazarmoneyapi/controller/PessoaController.java
@@ -2,9 +2,12 @@ package com.salazar.salazarmoneyapi.controller;
 
 import com.salazar.salazarmoneyapi.domain.model.Pessoa;
 import com.salazar.salazarmoneyapi.domain.repository.PessoaRepository;
+import com.salazar.salazarmoneyapi.event.RecursoCriadoEvent;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,15 +20,15 @@ public class PessoaController {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @PostMapping
     public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(pessoaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
     }
 
     @GetMapping("/{codigoId}")
